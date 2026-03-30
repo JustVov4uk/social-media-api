d404504c55946ceb3a21b30e70067e55ddcedb32
JustVov4uk <volodabudzan4@gmail.com>
2026-03-30 12:07:49 +0200

    feat: add User and Profile models with email-based authentication

--- user/models.py
+++ user/models.py
@@ -1,3 +1,71 @@
-from django.db import models
+import pathlib
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import UserManager as DjangoUserManager
+from django.db import models
+from django.utils.text import slugify
+from django.utils.translation import gettext as _
+
+
+class UserManager(DjangoUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The email must be set")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+
+def avatar_image_path(instance: "Profile", filename: str) -> pathlib.Path:
+    filename = (
+            f"{slugify(instance.user.username)}-{uuid.uuid4()}"
+            + pathlib.Path(filename).suffix
+    )
+    return pathlib.Path("upload/avatars/") / pathlib.Path(filename)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    date_of_birth = models.DateField(blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True)
+    biography = models.TextField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_private = models.BooleanField(default=False)
+    avatar = models.ImageField(upload_to=avatar_image_path, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
+
+    def __str__(self):
+        return self.user.username
