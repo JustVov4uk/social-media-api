62fb0340d3bf9f6a1e72c7a74f61d34027cd16b2
JustVov4uk <volodabudzan4@gmail.com>
2026-04-05 15:05:25 +0200

    feat: add retrieve profile by user endpoint

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 
-from user.views import UserRegisterViewSet, LogoutView, RetrieveUpdateProfileAPIView
+from user.views import UserRegisterViewSet, LogoutView, RetrieveUpdateProfileAPIView, RetrieveProfileAPIView
 
 urlpatterns = [
     path("register/", UserRegisterViewSet.as_view(), name="register"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("profile/", RetrieveUpdateProfileAPIView.as_view(), name="profile"),
+    path("profile/<int:pk>/", RetrieveProfileAPIView.as_view(), name="profile-detail"),
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,8 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from user.models import Profile
 from user.serializers import UserSerializer, UserRegisterSerializer, ProfileSerializer
 
 
@@ -27,3 +29,11 @@ class RetrieveUpdateProfileAPIView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user.profile
+
+
+class RetrieveProfileAPIView(generics.RetrieveAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return get_object_or_404(Profile, user=self.kwargs["pk"])
