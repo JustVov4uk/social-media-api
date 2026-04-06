5c25bc14fb83d52b51d8e2778da68be0078c2dc4
JustVov4uk <volodabudzan4@gmail.com>
2026-04-06 12:44:45 +0200

    feat: add follow/unfollow and followers/following endpoints

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
 from user.views import UserRegisterViewSet, LogoutView, RetrieveUpdateProfileAPIView, RetrieveProfileAPIView, \
-    ListProfileAPIView
+    ListProfileAPIView, FollowViewSet
 
+router = DefaultRouter()
+router.register("follow", FollowViewSet, basename="follow")
 urlpatterns = [
+    path("", include(router.urls)),
     path("register/", UserRegisterViewSet.as_view(), name="register"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("profile/", RetrieveUpdateProfileAPIView.as_view(), name="profile"),
--- user/views.py
+++ user/views.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import action
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, generics, status
@@ -5,7 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from user.models import Profile
+from user.models import Profile, User
 from user.serializers import UserSerializer, UserRegisterSerializer, ProfileSerializer
 
 
@@ -52,3 +53,58 @@ class ListProfileAPIView(generics.ListAPIView):
                 Q(user__username__icontains=search) | Q(user__email__icontains=search)
             )
         return queryset
+
+
+class FollowViewSet(viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+    @action(
+        detail=True,
+        methods=["post"],
+        permission_classes=(IsAuthenticated,),
+    )
+    def follow(self, request, pk):
+        user = self.get_object()
+        if request.user == user:
+            return Response(
+                {"error": "You cannot follow yourself"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        request.user.following.add(user)
+        return Response(status=status.HTTP_200_OK)
+
+    @action(
+        detail=True,
+        methods=["delete"],
+        permission_classes=(IsAuthenticated,),
+    )
+    def unfollow(self, request, pk):
+        user = self.get_object()
+        if request.user == user:
+            return Response(
+                {"error": "You cannot unfollow yourself"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        request.user.following.remove(user)
+        return Response(status=status.HTTP_200_OK)
+
+    @action(
+        detail=False,
+        methods=["get"],
+        permission_classes=(IsAuthenticated,),
+    )
+    def followers(self, request):
+        followers = request.user.followers.all()
+        serializer = UserSerializer(followers, many=True)
+        return Response(serializer.data)
+
+    @action(
+        detail=False,
+        methods=["get"],
+        permission_classes=(IsAuthenticated,),
+    )
+    def following(self, request):
+        following = request.user.following.all()
+        serializer = UserSerializer(following, many=True)
+        return Response(serializer.data)
